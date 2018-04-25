43af2a9af672b5af10c9adc17b58e52a67f36fa3
linkedhorizon <1054081981@qq.com>
2018-04-25 13:47:26 +0800

    对jedis进行封装，提供对外的工具类RedisUtil

---
+++ redis-ocean/src/main/java/org/arctic/redis/util/PropertiesUtil.java
@@ -0,0 +1,41 @@
+package org.arctic.redis.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Properties;
+
+/**
+ * Created by Administrator on 2018/4/25 0025.
+ */
+public class PropertiesUtil {
+    private static Properties props;
+
+    static {
+        String fileName = "redis.properties";
+        props = new Properties();
+        try {
+            props.load(new InputStreamReader(PropertiesUtil.class.getClassLoader().getResourceAsStream(fileName),"UTF-8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key){
+        String value = props.getProperty(key.trim());
+        if(StringUtils.isBlank(value)){
+            return null;
+        }
+        return value.trim();
+    }
+
+    public static String getProperty(String key,String defaultValue){
+
+        String value = props.getProperty(key.trim());
+        if(StringUtils.isBlank(value)){
+            value = defaultValue;
+        }
+        return value.trim();
+    }
+}
