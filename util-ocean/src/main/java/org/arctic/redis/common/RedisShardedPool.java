eff5eb5733e6e926f5caf976a1b7e08cdce2dc85
linkedhorizon <1054081981@qq.com>
2018-04-28 09:47:11 +0800

    编写RedisShardedUtil

--- util-ocean/src/main/java/org/arctic/redis/common/RedisShardedPool.java
+++ util-ocean/src/main/java/org/arctic/redis/common/RedisShardedPool.java
@@ -1,7 +1,10 @@
 package org.arctic.redis.common;
 
 import org.arctic.redis.util.PropertiesUtil;
-import redis.clients.jedis.*;
+import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.JedisShardInfo;
+import redis.clients.jedis.ShardedJedis;
+import redis.clients.jedis.ShardedJedisPool;
 import redis.clients.util.Hashing;
 import redis.clients.util.Sharded;
 
@@ -9,18 +12,24 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by Administrator on 2018/4/25 0025.
+ * Created by Administrator on 2018/4/28 0028.
  */
-public class RedisPool {
-    private static JedisPool jedisPool;//jedis连接池
+/**
+ * Created by Administrator on 2018/4/28 0028.
+ */
+public class RedisShardedPool {
+    private static ShardedJedisPool shardedJedisPool;//sharded jedis连接池
     private static Integer maxTotal = Integer.parseInt(PropertiesUtil.getProperty("redis.max.total","20"));//最大连接数
     private static Integer maxIdle = Integer.parseInt(PropertiesUtil.getProperty("redis.max.idle","10"));//在jedispool中最大的idle状态（空闲的）的jedis实例的个数
     private static Integer minIdle = Integer.parseInt(PropertiesUtil.getProperty("redis.min.idle","2"));//在jedispool中最小的idle状态（空闲的）的jedis实例的个数
     private static Boolean testOnBorrow = Boolean.parseBoolean(PropertiesUtil.getProperty("redis.test.borrow","true"));//在borrow一个jedis实例的时候，是否要进行验证操作，如果赋值true，则得到的jedis实例是可以用的
     private static Boolean testOnReturn = Boolean.parseBoolean(PropertiesUtil.getProperty("redis.test.return","true"));//在return一个jedis实例的时候，是否要进行验证操作，如果赋值true，则得到的jedis实例是可以用的
 
-    private static String ip = PropertiesUtil.getProperty("redis.ip");
-    private static Integer port = Integer.parseInt(PropertiesUtil.getProperty("redis.port"));
+    private static String ip1 = PropertiesUtil.getProperty("redis.ip1");
+    private static Integer port1 = Integer.parseInt(PropertiesUtil.getProperty("redis.port1"));
+
+    private static String ip2 = PropertiesUtil.getProperty("redis.ip2");
+    private static Integer port2= Integer.parseInt(PropertiesUtil.getProperty("redis.port2"));
     private static void initPool(){
         JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
 
@@ -33,32 +42,40 @@ public class RedisPool {
 
         jedisPoolConfig.setBlockWhenExhausted(true);//连接耗尽的时候，是否阻塞，false会抛出异常，true阻塞直到超时
 
-        jedisPool = new JedisPool(jedisPoolConfig,ip,port,1000*2);
+        JedisShardInfo info1 = new JedisShardInfo(ip1,port1,1000*2);
+        JedisShardInfo info2 = new JedisShardInfo(ip2,port2,1000*2);
+
+        List<JedisShardInfo> jedisShardInfoList = new ArrayList<JedisShardInfo>(2);
+        jedisShardInfoList.add(info1);
+        jedisShardInfoList.add(info2);
+        shardedJedisPool = new ShardedJedisPool(jedisPoolConfig, jedisShardInfoList, Hashing.MURMUR_HASH, Sharded.DEFAULT_KEY_TAG_PATTERN);
     }
     static {
         initPool();
     }
 
-    public static Jedis getJedis(){
-        return jedisPool.getResource();
+    public static ShardedJedis getJedis(){
+        return shardedJedisPool.getResource();
     }
 
-    public static void returnBrokenResource(Jedis resource){
-            jedisPool.returnBrokenResource(resource);
+    public static void returnBrokenResource(ShardedJedis resource){
+        shardedJedisPool.returnBrokenResource(resource);
     }
 
-    public static void returnResource(Jedis resource){
-            jedisPool.returnResource(resource);
+    public static void returnResource(ShardedJedis resource){
+        shardedJedisPool.returnResource(resource);
     }
 
     public static void main(String[] args){
-        Jedis jedis = RedisPool.getJedis();
-        jedis.set("arctic","北冰洋");
+        ShardedJedis shardedJedis = RedisShardedPool.getJedis();
+        for(int i = 0;i<10;i++){
+            shardedJedis.set("key"+i,"value"+i);
+        }
+        returnResource(shardedJedis);
 
-        returnResource(jedis);
-
-        jedisPool.destroy();//临时调用，销毁连接池中的所有连接
+        //jedisPool.destroy();//临时调用，销毁连接池中的所有连接
         System.out.println("program is end");
 
     }
 }
+
---
+++ util-ocean/src/main/java/org/arctic/redis/util/RedisShardedPoolUtil.java
@@ -0,0 +1,105 @@
+package org.arctic.redis.util;
+
+
+import org.arctic.redis.common.RedisShardedPool;
+import redis.clients.jedis.ShardedJedis;
+
+
+/**
+ * Created by Administrator on 2018/4/28 0028.
+ */
+public class RedisShardedPoolUtil {
+    public static String set(String key,String value){
+        ShardedJedis jedis = null;
+        String result = null;
+        try {
+            jedis = RedisShardedPool.getJedis();
+            result = jedis.set(key,value);
+        } catch (Exception e) {
+            e.printStackTrace();
+            RedisShardedPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisShardedPool.returnResource(jedis);
+        return result;
+    }
+
+    public static String get(String key){
+        ShardedJedis jedis = null;
+        String result = null;
+
+        try {
+            jedis = RedisShardedPool.getJedis();
+            result = jedis.get(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+            RedisShardedPool.returnBrokenResource(jedis);
+            return  result;
+        }
+        RedisShardedPool.returnResource(jedis);
+        return result;
+    }
+
+    //exTime单位是秒
+    public static String setEx(String key,String value,int exTime){//设置有效期
+        ShardedJedis jedis = null;
+        String result = null;
+        try {
+            jedis = RedisShardedPool.getJedis();
+            result = jedis.setex(key,exTime,value);
+        } catch (Exception e) {
+            e.printStackTrace();
+            RedisShardedPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisShardedPool.returnResource(jedis);
+        return result;
+    }
+
+    /**
+     * 设置key的有效期，单位是秒
+     * @param key
+     * @param exTime
+     * @return
+     */
+    public static Long expire(String key,int exTime){//重新设置有效期
+        ShardedJedis jedis = null;
+        Long result = null;
+        try {
+            jedis = RedisShardedPool.getJedis();
+            result = jedis.expire(key,exTime);
+        } catch (Exception e) {
+            e.printStackTrace();
+            RedisShardedPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisShardedPool.returnResource(jedis);
+        return result;
+    }
+
+    /**
+     * 删除key，value
+     * @param key
+     * @return
+     */
+    public Long del(String key){
+        ShardedJedis jedis = null;
+        Long result = null;
+        try {
+            jedis = RedisShardedPool.getJedis();
+            result = jedis.del(key);
+        } catch (Exception e) {
+            e.printStackTrace();
+            RedisShardedPool.returnBrokenResource(jedis);
+            return result;
+        }
+        RedisShardedPool.returnResource(jedis);
+        return result;
+    }
+
+    public static void main(String[] args){
+        String arctic = RedisPoolUtil.get("arctic");
+        RedisPoolUtil.expire("arctic",100);
+        System.out.println(arctic);
+    }
+}
